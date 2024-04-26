2938df24b71b47cf081a631107121c5792ae7c6e
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-26 17:28:19 +0900

    add some tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -14,7 +14,10 @@ let add = fn(x, y) {
 	x + y;
 };
 
-let result = add(five, ten);`
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
--- token/token.go
+++ token/token.go
@@ -8,25 +8,46 @@ type Token struct {
 }
 
 const (
+	// ILLEGAL はトークンが未知の文字を含んでいることを示します。
 	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
-
+	// EOF は入力の終端を示します。
+	EOF = "EOF"
+	// IDENT は識別子を示します。
 	IDENT = "IDENT"
-	INT   = "INT"
-
+	// INT は整数リテラルを示します。
+	INT = "INT"
+	// ASSIGN は代入演算子を示します。
 	ASSIGN = "="
-	PLUS   = "+"
-
-	COMMA     = ","
+	// PLUS は加算演算子を示します。
+	PLUS = "+"
+	// MINUS は減算演算子を示します。
+	MINUS = "-"
+	// BANG はビット反転演算子を示します。
+	BANG = "!"
+	// ASTERISK は乗算演算子を示します。
+	ASTERISK = "*"
+	// SLASH は除算演算子を示します。
+	SLASH = "/"
+	// LT は小なり演算子を示します。
+	LT = "<"
+	// GT は大なり演算子を示します。
+	GT = ">"
+	// COMMA はカンマを示します。
+	COMMA = ","
+	// SEMICOLON はセミコロンを示します。
 	SEMICOLON = ";"
-
+	// LPAREN は左括弧を示します。
 	LPAREN = "("
+	// RPAREN は右括弧を示します。
 	RPAREN = ")"
+	// LBRACE は左波括弧を示します。
 	LBRACE = "{"
+	// RBRACE は右波括弧を示します。
 	RBRACE = "}"
-
+	// FUNCTION は関数を示します。
 	FUNCTION = "function"
-	LET      = "let"
+	// LET は変数束縛を示します。
+	LET = "let"
 )
 
 var keywords = map[string]TokenType{
