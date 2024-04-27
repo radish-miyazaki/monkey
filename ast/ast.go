f1a8a427c3c86fa30c80026c00b361ec758c5108
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-27 22:37:16 +0900

    impl Stringer interface to AST

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/radish-miyazaki/monkey/token"
+import (
+	"bytes"
+
+	"github.com/radish-miyazaki/monkey/token"
+)
 
 // Node は全てのASTノードが実装するインターフェース
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement は全ての文を表すASTノードのインターフェース
@@ -31,6 +36,21 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
@@ -42,6 +62,39 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Expression.TokenLiteral()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
 // Identifier は識別子を表すASTノード
 // 本来 let 文の識別子は値を生成しないが、ノードの種類を少なくするために Expression を実装している
 type Identifier struct {
@@ -49,10 +102,14 @@ type Identifier struct {
 	Value string // 識別子名
 }
 
-func (i *Identifier) statementNode() {}
+func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 // Program は全てのASTノードのルートノードとなるもの
@@ -66,4 +123,13 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
