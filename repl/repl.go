1ecd98c0ce24544c3da2bdb259f86163aa8dc106
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-28 19:23:33 +0900

    improve repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,12 +6,13 @@ import (
 	"io"
 
 	"github.com/radish-miyazaki/monkey/lexer"
+	"github.com/radish-miyazaki/monkey/parser"
 	"github.com/radish-miyazaki/monkey/token"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, _ io.Writer) {
+func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
@@ -23,9 +24,41 @@ func Start(in io.Reader, _ io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		_, _ = io.WriteString(out, program.String())
+		_, _ = io.WriteString(out, "\n")
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
+	}
+}
+
+const MonkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParseErrors(out io.Writer, errors []string) {
+	_, _ = io.WriteString(out, MonkeyFace)
+	_, _ = io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
+	_, _ = io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		_, _ = io.WriteString(out, "\t"+msg+"\n")
 	}
 }
